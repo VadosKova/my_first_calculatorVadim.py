ae9c363a210ccf8191355663576e9e0a77f0f7cc
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-02-12 09:56:14 +0200

    Added file for history

--- my_first_calculator.py
+++ my_first_calculator.py
@@ -4,6 +4,8 @@
 if 3/2 == 1:  # Because Python 2 does not know maths
     input = raw_input  # Python 2 compatibility
 
+history = "history.txt"
+
 print('Welcome to this calculator!')
 print('It can add, subtract, multiply and divide whole numbers from 0 to 50')
 num1 = int(input('Please choose your first number: '))
